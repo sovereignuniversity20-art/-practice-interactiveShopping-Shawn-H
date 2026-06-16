5866a93cca63dd1a7712d98f21c92f835a6a0648
Hero <sovereign.university20@gmail.com>
2026-06-16 08:55:54 -0500

    first update on group project

---
+++ script.js
@@ -0,0 +1,27 @@
+const product = document.getElementById("addProduct");
+const button = document.getElementById("myButton");
+const list = document.getElementById("productlist");
+//const newItem = document.createElement(addproduct);
+
+//function newProduct(addproduct) {
+  //      list.push(addproduct);
+       // return list; 
+        //onsole.log(`You added: ${addproduct}`);
+    //}
+
+list.addEventListener("input", () =>
+list.textContent = (`You added: ${addproduct}`)
+);
+
+document.forms.appendChild(product)
+
+//button.addEventListener('input', function(event) {
+  //      output.textContent = `You added: ${product}`;
+    //  });
+
+
+//button.addEventListener("click", handleClick);
+
+//button.addEventListener('click', () => {
+//addProduct();
+//})
