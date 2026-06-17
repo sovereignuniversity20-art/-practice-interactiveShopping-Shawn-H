3ec83750843432f08b05a3099e87026e671cd4da
Hero <sovereign.university20@gmail.com>
2026-06-16 23:36:56 -0500

    everything working

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const button = document.getElementById("addButton");
 button.addEventListener("click", function(event) {
     const newItem = product.value;
     const newEntry = document.createElement("li");
-    newEntry.textContent = (`${newItem}`);
+    const newInfo = document.createElement("span");
+    newInfo.textContent = (newItem);
+    newEntry.appendChild(newInfo);
     const removeItem = document.createElement("button");
     const editItem = document.createElement("button");
     removeItem.textContent = ("Remove"); 
@@ -14,11 +16,36 @@ button.addEventListener("click", function(event) {
     newEntry.appendChild(removeItem);
     newEntry.appendChild(editItem);
     product.value = "";
-
+    let isEditing = false;
+    let editedItem;
 removeItem.addEventListener("click", function(event) {
     newEntry.remove();
-})});
+    })
 
+editItem.addEventListener("click", function(event) {
+    
+    if (isEditing === false) {
+        editedItem = document.createElement("textarea");
+        editedItem.value = newInfo.textContent;
+        newEntry.appendChild(editedItem);
+        editItem.textContent = ("Save");
+        isEditing = true;
+    } else {
+        newInfo.textContent = editedItem.value;
+        editedItem.remove();
+        editItem.textContent = ("Edit");  
+        isEditing = false;
+    }
+});    
+});
+//editItem.textContent = ("Save");
+    //newEntry.appendChild(editedItem);
+
+    //editItem.addEventListener("click", function(event)  {
+    //newInfo.textContent = editedItem.value; 
+    //editedItem.remove(); 
+    //editItem.textContent = ("Edit");  
+ 
 /*editItem.addEventListener("click", fucntion(event) {
   //  newEntry.textContent = (`${newItem}`);
 });
@@ -32,3 +59,4 @@ removeItem.addEventListener("click", function(event) {
     product.value = "";
     button.removeEventListener("click", handleClick);
 })*/
+
